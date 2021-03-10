578a6c9e0b9cc8d4491fca2c920bb302eb28f48e
Filip <f-r-c@LAPTOP-BO73C4EV>
2021-03-10 15:52:41 +0100

    Add project files.

---
+++ SWD-STONKS/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Security.Cryptography.X509Certificates;
+
+namespace SWD_STONKS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+   public interface ISubject
+    {
+        public void Attach(IObserver observer);
+        public void Detach(IObserver observer);
+        public void Notify();
+    }
+
+   public interface IObserver
+   {
+       public void Update(ISubject subject);
+   }
+
+   public class Portfolio :IObserver
+   {
+
+   }
+}
---
+++ SWD-STONKS/Stock.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SWD_STONKS
+{
+    public class Stock : ISubject
+    {
+        private ICollection<IObserver> observers;
+        public string Name { get; set; }
+        public double Value { get; set; }
+
+        public Stock()
+        {
+            this.observers = new List<IObserver>();
+        }
+
+        public void Attach(IObserver observer)
+        {
+            observers.Add(observer);
+        }
+
+        public void Detach(IObserver observer)
+        {
+            observers.Remove(observer);
+        }
+
+        public void Notify()
+        {
+            for (int i = 0; i < observers.Count; i++)
+            {
+                observers.ElementAt(i).Update(this);
+            }
+        }
+    }
+}
