ac34312992e8b79e39ee2af939f0a93a0f0d2a56
AnnSo <64138186+auAnnSo@users.noreply.github.com>
2021-03-11 11:11:08 +0100

    Stock -> removed methods Subject -> StockSubject -> added methods IObserver -> ISubject to StockSubject

---
+++ SWD-STONKS/StockSubject.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace SWD_STONKS
+{
+    public abstract class StockSubject
+    {
+        private ICollection<IObserver> observers = new List<IObserver>();
+
+
+        public void Attach(IObserver observer)
+        {
+            observers.Add(observer);
+        }
+
+        public void Detach(IObserver observer)
+        {
+            observers.Remove(observer);
+        }
+
+        public void Notify()
+        {
+            foreach(IObserver observer in observers)
+            {
+                observer.Update(this);
+            }
+        }
+
+    }
+}
--- SWD-STONKS/Subject.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SWD_STONKS
-{
-    public abstract class Subject
-    {
-        public abstract void Attach(IObserver observer);
-        public abstract void Detach(IObserver observer);
-        public abstract void Notify();
-    }
-}
